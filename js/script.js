4dd2982066a0eedb95f9d7f678b1026896e622d5
Anto231996 <tony2396@hotmail.it>
2022-03-17 01:08:29 +0100

    aggiunta img

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ let app = new Vue(
     {
         el: '#app',
         data: {
-            message:'Hello Vue'
+            message:'Hello Vue', 
+            myImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
         }
     }
 );    
